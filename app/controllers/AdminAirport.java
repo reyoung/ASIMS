43b5025989abf7d0ad53b1aa8622b18e5effe22c
reyoung <reyoung@126.com>
2013-03-15 01:59:27 +0800

    Query Basically Complete.

--- app/controllers/AdminAirport.java
+++ app/controllers/AdminAirport.java
@@ -1,6 +1,12 @@
 package controllers;
 
+import models.Airport;
+import models.Page;
+import play.Logger;
+import play.data.validation.Required;
 import play.mvc.Controller;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,7 +16,19 @@ import play.mvc.Controller;
  * To change this template use File | Settings | File Templates.
  */
 public class AdminAirport extends Controller {
-    public static void list(int page,int pageSize) {
-        render();
+    public static void list(Integer page,Integer pageSize, String filter) {
+        if (page == null){
+            page = 1;
+        }
+        if (pageSize==null){
+            pageSize = 10;
+        }
+        if (filter == null){
+            filter = "%";
+        }
+        List<Airport> airports= Airport.find("byNameLike",filter).fetch(page,pageSize);
+        long all = Airport.count();
+        Page<Airport> pages = new Page<Airport>(airports,page,pageSize,all);
+        render(pages);
     }
 }
