89f13aaeee7f47fb1f1c6009ca6bfe83922c3ee7
reyoung <reyoung@126.com>
2013-03-15 01:05:31 +0800

    Admin UI Framework Complete.

---
+++ app/controllers/AdminAirport.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: reyoung
+ * Date: 3/14/13
+ * Time: 11:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AdminAirport extends Controller {
+    public static void list(int page,int pageSize) {
+        render();
+    }
+}
---
+++ app/models/Airport.java
@@ -0,0 +1,48 @@
+package models;
+
+import play.data.validation.MaxSize;
+import play.data.validation.MinSize;
+import play.data.validation.Required;
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: reyoung
+ * Date: 3/14/13
+ * Time: 11:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity(name = "Airport")
+public class Airport extends Model {
+    @MinSize(2)
+    @Required
+    @MaxSize(255)
+    public String Name;
+
+    @Required
+    public int CityId;
+
+    @Required
+    public int CountryId;
+
+    static public String[][] CountryCityTable = {
+            {"中国","天津","上海"},
+            {"美国","纽约","新奥尔良","休斯顿"}
+    };
+
+    public String getCity(){
+        return CountryCityTable[this.CountryId][this.CityId+1];
+    }
+
+    public String getCountry(){
+        return CountryCityTable[this.CountryId][this.CityId];
+    }
+
+    public Airport(String name, int cityId, int countryId) {
+        Name = name;
+        CityId = cityId;
+        CountryId = countryId;
+    }
+}
