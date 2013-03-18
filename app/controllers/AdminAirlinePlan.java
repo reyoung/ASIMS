c53df884911af94cdcd56ebba1402b5e59626eee
reyoung <reyoung@126.com>
2013-03-18 13:54:53 +0800

    Add Debug To AirlinePlan to add stub data

--- app/controllers/AdminAirlinePlan.java
+++ app/controllers/AdminAirlinePlan.java
@@ -1,10 +1,12 @@
 package controllers;
 
+import models.AirlinePlan;
 import models.Airport;
 import play.mvc.Controller;
 
+import java.sql.Timestamp;
 import java.util.List;
-
+//import fj.data.List;
 /**
  * Created with IntelliJ IDEA.
  * User: reyoung
@@ -18,4 +20,15 @@ public class AdminAirlinePlan  extends Controller{
         render(airports);
     }
 
+    public static void debug(){
+        List<Airport> aps = Airport.all().fetch(2);
+        AirlinePlan alp  = new AirlinePlan();
+        alp.FlyTime = 60;
+        alp.Repeat="N";
+        alp.LeaveTime = new Timestamp(2012,12,21,10,10,10,10);
+        alp.LeavePlace = aps.get(0);
+        alp.ArrivePlace = aps.get(1);
+        alp.Number = "CZ300212";
+        alp.save();
+    }
 }
