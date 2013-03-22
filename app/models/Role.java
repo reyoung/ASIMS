033f6daab853da8eedd2e05d567e936849be56e7
shit <z498324116@126.com>
2013-03-22 14:50:12 +0800

    Role 改版

--- app/models/Role.java
+++ app/models/Role.java
@@ -17,22 +17,26 @@ import play.db.jpa.Model;
 public class Role extends Model{
 	@Column(name = "Name", nullable = false, length = 255)
 	public String Name;
-	@Column(name = "Privilege", nullable = false, length = 255)
-	public String Privilege;
+	@Column(name = "Privilege", nullable = false)
+	public int Privilege;
+	
+	public class PrivilegeType {
+		public boolean Readable;
+		public boolean Writable;
+		public PrivilegeType(int privilege, int pos){
+			//* Set Readable and Writable
+			privilege >>= pos * 2;
+			privilege &= 3;
+			this.Readable = (privilege & 1)!=0;
+			this.Writable = (privilege & 2)!=0;
+		}
+	}
 	
 	
-	public String getName() {
-		return Name;
-	}
-	public void setName(String name) {
-		Name = name;
-	}
-	public String getPrivilege() {
-		return Privilege;
-	}
-	public void setPrivilege(String privilege) {
-		Privilege = privilege;
-	}
+	public static int USER_TABLE_POS = 0;
 	
+	public PrivilegeType getUserTablePrivilege(){
+		return new PrivilegeType(Privilege, USER_TABLE_POS);
+	}
 
 }
