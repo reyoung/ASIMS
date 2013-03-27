315e7171716ce1be97acca15b5dde1eec79b19c3
reyoung <reyoung@126.com>
2013-03-27 16:11:24 +0800

    create.html Complete

---
+++ app/controllers/AdminPropertyResource.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: reyoung
+ * Date: 13-3-27
+ * Time: 下午3:41
+ * EMail: reyoung@126.com
+ * Blog: www.reyoung.me
+ */
+public class AdminPropertyResource extends Controller {
+    public static void create(){
+        render();
+    }
+}
