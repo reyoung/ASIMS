a7a59956f67d5c7cb9f4149c481804c584aa0881
reyoung <reyoung@126.com>
2013-03-27 16:22:30 +0800

    handle edit complete

--- app/controllers/AdminPropertyResource.java
+++ app/controllers/AdminPropertyResource.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import models.Facility;
+import play.data.validation.Required;
+import play.data.validation.Validation;
 import play.mvc.Controller;
 
 /**
@@ -13,5 +16,30 @@ import play.mvc.Controller;
 public class AdminPropertyResource extends Controller {
     public static void create(){
         render();
+    }
+
+    public static void list(Integer page, Integer pageSize, String filter){
+
+    }
+
+
+    public static void handleCreate(@Required String Name,
+                                    @Required String Position,
+                                    @Required Integer Amount,
+                                    @Required String Telephone,
+                                    String Comment){
+        if(Validation.hasErrors()) badRequest();
+        Facility fc = new Facility();
+        fc.Name = Name;
+        fc.Position = Position;
+        fc.Amount = Amount;
+        fc.Telephone = Telephone;
+        fc.Comment = Comment;
+        boolean  ok = fc.validateAndCreate();
+        if (!ok){
+            badRequest();
+        } else {
+            list(null,null,null);
+        }
     }
 }
