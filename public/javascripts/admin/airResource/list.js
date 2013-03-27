c1b7ed6fc3e51fa91f0348bc0af4ef16684c8869
reyoung <reyoung@126.com>
2013-03-27 15:15:31 +0800

    AirportResource List Complete. Add Some initial-data

--- public/javascripts/admin/airResource/list.js
+++ public/javascripts/admin/airResource/list.js
@@ -1,7 +1,7 @@
 require(["admin/active","admin/delete"],function(act,opt){
     $(function (){
     	act("nav_airportResource_list") //! Active The Navigation List.
-    	opt("/admin/AirportResource/");
+    	opt("/admin/AirlinePlan/");
     });
 
 })
