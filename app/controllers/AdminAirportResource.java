f4372730c10f3524b1ef15f7ffd65cca0565d1db
reyoung <reyoung@126.com>
2013-03-27 14:53:23 +0800

    Fix Bug In AdminRole when Handle Edit.
    Refine AirportResource.

--- app/controllers/AdminAirportResource.java
+++ app/controllers/AdminAirportResource.java
@@ -1,9 +1,12 @@
 package controllers;
 
 import models.Facility;
+import models.Page;
 import play.data.validation.Required;
 import play.data.validation.Validation;
 import play.mvc.Controller;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,6 +20,14 @@ public class AdminAirportResource extends Controller {
     public static void create(){
         render();
     }
+    public static void edit(Long id){
+        render();
+    }
+
+    public static void delete(Long id){
+        renderJSON(false);
+    }
+
     public static void handleCreate(@Required String Name,
                                     @Required String Position,
                                     @Required Integer Amount, String Comment){
@@ -33,10 +44,22 @@ public class AdminAirportResource extends Controller {
         if (!ok ){
             badRequest();
         } else {
-            list(null,null);
+            list(null,null,null);
         }
     }
-    public static void list(Integer page, Integer pageSize){
-        render();
+    public static void list(Integer page, Integer pageSize, String filter){
+        if (page == null || page <1){
+            page = 1;
+        }
+        if (pageSize == null || pageSize<1){
+            pageSize = 10;
+        }
+
+        List<Facility> model = Facility.find("Type >= ? and Type < ?" ,Facility.AirportResourceType,Facility.PropertyResourceType)
+                .fetch(page,pageSize);
+        Page<Facility> pages = new Page<Facility>(model,page,pageSize,
+                Facility.count("Type >= ? and Type < ?",Facility.AirportResourceType,Facility.PropertyResourceType)
+                );
+        render(pages);
     }
 }
