985491c833a8caae0310678cd8d4d45932a17383
reyoung <reyoung@126.com>
2013-03-27 18:24:44 +0800

    Start PropertyResource list.html

--- app/controllers/AdminPropertyResource.java
+++ app/controllers/AdminPropertyResource.java
@@ -1,9 +1,12 @@
 package controllers;
 
 import models.Facility;
+import models.Page;
 import play.data.validation.Required;
 import play.data.validation.Validation;
 import play.mvc.Controller;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,7 +22,22 @@ public class AdminPropertyResource extends Controller {
     }
 
     public static void list(Integer page, Integer pageSize, String filter){
-
+        if(page==null || page <1){
+            page = 1;
+        }
+        if(pageSize==null || pageSize<1){
+            pageSize = 10;
+        }
+        if(filter == null) {
+            filter = "";
+        }
+        filter = "%"+filter+"%";
+        List<Facility> data = Facility.find("Type >=? and Name like ?",Facility.PropertyResourceType,filter)
+                                .fetch(page, pageSize);
+        Page<Facility> pages =
+                new Page<Facility>(data,page,pageSize,
+                Facility.count("Type >=? and Name like ?",Facility.PropertyResourceType,filter));
+        render(pages);
     }
 
 
