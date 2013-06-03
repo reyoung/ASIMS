18277c11d2d083347b6366ee1b78fa912a5bd6d9
reyoung <reyoung@126.com>
2013-06-03 14:25:53 +0800

    JS Check For User Complete.

--- public/javascripts/admin/user/create.js
+++ public/javascripts/admin/user/create.js
@@ -1,3 +1,70 @@
-require(["admin/active","field_checker"],function(act,field){
-    act("nav_user_create")
+require(["admin/active","field_checker"],function(act,fc){
+    act("nav_user_list")
+    $(function(){
+        fc.def(["LoginName","Password","PasswordRepeat","Email","UserNumber","Telephone"])
+        $("form").submit(function(){
+            var ok = fc.chk("LoginName",function(text){
+                if(text.length==0){
+                    return "登录名称不能为空"
+                } else if(text.length>255){
+                    return "登录名称不能长于255字符"
+                } else {
+                    return true
+                }
+            })
+            ok = ok && fc.chk("Password",function(pwd){
+                if(pwd.length==0){
+                    return "密码不能为空"
+                } else if(pwd.length>255){
+                    return "密码不能超过255字符"
+                } else {
+                    return true
+                }
+            })
+            ok = ok && fc.chk("PasswordRepeat",function(pwd){
+                if(pwd!==$("#Password").val()){
+                    return "两次输入的密码不一致"
+                } else {
+                    return true
+                }
+            })
+            function isEmail(strEmail) {
+                if (strEmail.search(/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/) != -1)
+                    return true
+                else
+                    return false
+            }
+            ok = ok && fc.chk("Email",function (email){
+                if(!isEmail(email)){
+                    return "请输入正确格式的Email地址"
+                } else {
+                    return true
+                }
+            })
+            ok = ok && fc.chk("UserNumber",function(number){
+                if(number.length===0){
+                    return "工号不能为空"
+                } else {
+                    return true
+                }
+            })
+            function IsAllDigital(str){
+                for(var i=0;i<str.length;++i){
+                    if(isNaN(parseInt(str[i],10))
+                        return false
+                }
+                return true
+            }
+            function CheckTel(tel){
+                if(!IsAllDigital(tel)){
+                    return "请输入正确的电话号码"
+                } else {
+                    return true
+                }
+            }
+            ok = ok && fc.chk("Telephone",CheckTel)
+            ok = ok && fc.chk("Mobile",CheckTel)
+            return ok
+        })
+    })
 })
